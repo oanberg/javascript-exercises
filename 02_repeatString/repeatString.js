d4933cf044b1057b479be832ad6ffcefe05c4811
anberg <p.ottendahl@gmail.com>
2023-02-12 17:23:25 +0100

    pass first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+function repeatString(string, repetitions) {
+    let repeatedString = "";
+    if (repetitions < 0) {
+        repeatedString = "ERROR";
+    } else {
+    for (let i = 1; i<=repetitions;i++) {
+        repeatedString += string;
+    }
+}
+    return repeatedString
 
-};
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+function reverseString(string) {
+    let reversedString = "";
+    for (let i = 1; i <= string.length; i++) {
 
-};
+        reversedString += string[string.length - i]
+    }
+    return reversedString;
+    }
+
 
 // Do not edit below this line
 module.exports = reverseString;
