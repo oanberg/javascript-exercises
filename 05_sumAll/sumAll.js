f5295af2c10d84422958ca17b8e33f3c1e08ada7
anberg <p.ottendahl@gmail.com>
2023-02-12 22:13:16 +0100

    did exercise 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+function sumAll(min, max) {
+    let total = 0;
+    if (min < 0 || max <0 || typeof min != "number" || typeof max != "number") {
+        total = "ERROR";
+        return total
+    } else {
+        if (min > max){
+            for (let i = max; i <= min; i++) {
+                total += i;
+            } 
+        } else {
+    for (let i = min; i <= max; i++) {
+        //console.log(i);
+        total += i;
+        //console.log(total);
+    }
+}
+    return total
+}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
